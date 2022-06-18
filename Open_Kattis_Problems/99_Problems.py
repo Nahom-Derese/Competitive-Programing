f1bdb81d9cda620eef94630b1174e1578d3e4511
Nahom-Derese <nahomderese@gmail.com>
2022-06-18 07:19:00 +0300

    Two problems solved

--- Open_Kattis_Problems/99_Problems.py
+++ Open_Kattis_Problems/99_Problems.py
@@ -3,16 +3,14 @@ from math import trunc
 
 a = int(input())
 
-if log10(a).real > 1.0:
-    if a % 100 >= 49:
-        a = a // 100
-        ans = str(a) + "99"
-        a = int(ans)
-    else:
-        digit = trunc(log10(a).real)
-        b = a%100
-        a -= b+1
-else:
-    a = 99
 
-print(a)
+
+if trunc(log10(a).real) > 1.0:
+    if a%100 == 49:
+        a += 2
+    c = round(a,-2) - 1
+
+else:
+     c = 99
+
+print(c)
---
+++ Open_Kattis_Problems/Faktor.py
@@ -0,0 +1,3 @@
+a = [int(i) for i in input().split()]
+
+print((a[1] - 1) * a[0] + 1)
