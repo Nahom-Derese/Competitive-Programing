de1a496b914363313ee668e07cae8fc07b2f2ba7
Nahom-Derese <nahomderese@gmail.com>
2022-06-22 15:42:11 +0300

    Autori Solved using java

---
+++ Open_Kattis_Problems/Autori.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Autori
+ */
+public class Autori {
+
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        String[] input = n.nextLine().split("-");
+        for (int i = 0; i < input.length; i++) {
+            char ans = input[i].charAt(0);
+            System.out.print(ans);
+        }
+        System.out.print("\n");
+        n.close();
+    }
+}
