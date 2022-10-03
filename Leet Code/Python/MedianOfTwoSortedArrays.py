3666813c7ef97f124a17285d6a900bed6ce8a1cc
Nahom-Derese <nahomderese@gmail.com>
2022-10-03 11:24:42 +0300

    Leet Code ==> MedianOfTwoSortedArrays Solved with Python

---
+++ Leet Code/Python/MedianOfTwoSortedArrays.py
@@ -0,0 +1,12 @@
+import math
+from tokenize import Double
+from unicodedata import decimal
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        combined = [*nums1,*nums2]
+        combined.sort()
+        median = (combined[len(combined)//2 - 1] + combined[len(combined)//2]) / 2 if len(combined) % 2 == 0 else combined[len(combined)//2]
+        return float('{:.5f}'.format(median))
+
+print (Solution.findMedianSortedArrays(Solution, [1,3],[2,4]))
