40ac1c7c3b88292920727125bf2e7a075e5d6bfa
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-03-07 05:02:19 -0800

    Add solution for Shifting Letters II

---
+++ leetcode/shifting-letters-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        pre_sum = [0] * (len(s)+1)
+        mapp = ascii_lowercase
+
+        for a, b, c in shifts:
+            if c:
+                pre_sum[a] += 1
+                pre_sum[b+1] -= 1
+            else:
+                pre_sum[a] -= 1
+                pre_sum[b+1] += 1
+        
+        pre_sum = list(accumulate(pre_sum))
+        ans = ""
+        for i in range(len(s)):
+            ans += mapp[((ord(s[i]) - 97) + pre_sum[i]) % 26]
+        
+        return ans
