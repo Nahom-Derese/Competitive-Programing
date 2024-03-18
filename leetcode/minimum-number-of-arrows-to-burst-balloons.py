87db1be34a81e2d036415c5e4ef97c6b12a60c6d
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-03-18 07:52:03 -0700

    Add solution for Minimum Number of Arrows to Burst Balloons

--- leetcode/minimum-number-of-arrows-to-burst-balloons.py
+++ leetcode/minimum-number-of-arrows-to-burst-balloons.py
@@ -1,15 +1,14 @@
 class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort(key= lambda x: x[0])
+    def findMinArrowShots(self, points):
+        points.sort()
 
-        count = 1
-        _range = points[0]
-        for i,j in points[1:]:
-            print(_range)
-            if i <= _range[1]:
-                _range[0]=i
-                _range[1]=min(_range[1], j)
-            else:
-                _range = [i,j]
-                count+=1
-        return count
+        itr = ans = 0
+        while itr < len(points):
+            temp = itr
+            while itr < len(points) and points[temp][1] >= points[itr][0]:
+                if points[temp][1] > points[itr][1]:
+                    temp = itr
+                itr+=1
+            ans+=1
+
+        return ans
