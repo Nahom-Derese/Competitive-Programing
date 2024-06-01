a92ad8bf36d65b06a85c0c3af32e2c1f8caabd7f
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-06-01 07:52:49 +0300

    Created 'Maximum Depth of N-ary Tree' by A2SV Hub

---
+++ 01-Jun-2024/Maximum Depth of N-ary Tree 162929.py
@@ -0,0 +1,22 @@
+# Problem: Maximum Depth of N-ary Tree - https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        if not root.children:
+            return 1
+
+        max_depth = 0
+        for node in root.children:
+            max_depth = max(self.maxDepth(node), max_depth)
+        
+        return max_depth + 1
