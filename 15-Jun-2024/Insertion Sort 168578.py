dcfcb10e8a0f0d37f4363912e3ef42e2dc8b07cc
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-06-15 13:20:53 +0300

    Created 'Insertion Sort' by A2SV Hub

---
+++ 15-Jun-2024/Insertion Sort 168578.py
@@ -0,0 +1,36 @@
+# Problem: Insertion Sort - https://www.hackerrank.com/challenges/insertionsort1/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    
+    
+    num = arr[n-1]
+    j = n - 2
+    while j > -1 and num <= arr[j]:
+        arr[j+1] = arr[j]
+        j-=1
+        print(*arr)
+    arr[j+1] = num
+    print(*arr)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
