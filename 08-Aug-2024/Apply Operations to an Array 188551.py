264335d3032c296f04ed6ac5d2362d93fd4bcde3
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-08-08 22:41:25 +0300

    Created 'Apply Operations to an Array' by A2SV Hub

---
+++ 08-Aug-2024/Apply Operations to an Array 188551.py
@@ -0,0 +1,12 @@
+# Problem: Apply Operations to an Array - https://leetcode.com/problems/apply-operations-to-an-array/
+
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+             
+        reducedArray = [num for num in nums if num !=0]
+        return reducedArray + [0]* (n - len(reducedArray))
