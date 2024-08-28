ce1aa0744b58b5edd495c7cd8e5f92fd62dd5e81
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-08-28 12:48:01 +0300

    Created 'E - Word Transformation' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 28-Aug-2024/E - Word Transformation 208580.py
@@ -0,0 +1,22 @@
+# Problem: E - Word Transformation - https://codeforces.com/gym/543431/problem/E
+
+import string
+from collections import Counter
+
+def solve():
+    main, sub = [list(i) for i in input().split()]
+
+    main_count = Counter(main)
+    sub_count = Counter(sub)
+
+    for i in string.ascii_uppercase:
+        while main_count[i] > sub_count[i]:
+            main.remove(i)
+            main_count[i]-=1
+    
+    if main == sub:
+        return "YES"
+    return "NO"
+
+for _ in range(int(input())):
+    print(solve())
