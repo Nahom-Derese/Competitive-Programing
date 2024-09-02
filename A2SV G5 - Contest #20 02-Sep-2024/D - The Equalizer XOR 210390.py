f16698e460250a9e71e84f047d826a6b4214277f
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-09-02 17:39:33 +0300

    Created 'D - The Equalizer XOR' by A2SV Hub

---
+++ A2SV G5 - Contest #20 02-Sep-2024/D - The Equalizer XOR 210390.py
@@ -0,0 +1,27 @@
+# Problem: D - The Equalizer XOR - https://codeforces.com/gym/528792/problem/D
+
+def solve():
+    n = int(input())
+    nums = [int(i) for i in input().split()]
+    
+
+    prefix_XOR=[nums[0]]
+    for i in range(1,n):
+        prefix_XOR.append(prefix_XOR[-1]^nums[i])
+
+    # already if all are equal
+    if prefix_XOR[-1]==0:
+        return "YES"
+
+    for i in range(n):
+        first=prefix_XOR[i]
+        for j in range(i+1,n):
+            second=prefix_XOR[j]^first
+            third=prefix_XOR[-1]^prefix_XOR[j]
+            if first==second==third:
+                return "YES"
+
+    return "NO"
+
+for _ in range(int(input())):
+    print(solve())
