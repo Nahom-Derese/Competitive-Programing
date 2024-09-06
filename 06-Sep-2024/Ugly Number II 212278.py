49fe456651ed5c3d25cd61be363191eeef27e9d7
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-09-06 15:30:37 +0300

    Created 'Ugly Number II' by A2SV Hub

---
+++ 06-Sep-2024/Ugly Number II 212278.py
@@ -0,0 +1,16 @@
+# Problem: Ugly Number II - https://leetcode.com/problems/ugly-number-ii/
+
+class Solution(object):
+    def nthUglyNumber(self, n):
+        primes = [2,3,5]
+        uglyHeap = [1]
+        visited = set()
+        visited.add(1)
+        for _ in range(n):
+            curr = heappop(uglyHeap)
+            for prime in primes:
+                new_ugly = curr * prime
+                if new_ugly not in visited:
+                    heappush(uglyHeap, new_ugly)
+                    visited.add(new_ugly)
+        return curr
